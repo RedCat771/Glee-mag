19ba3677d8700ec1fa799616e38cbae5d575f021
RedCat771 <savelyev7979@gmail.com>
2020-12-05 18:55:26 +0200

    add shop pages

--- gulpfile.js
+++ gulpfile.js
@@ -53,10 +53,12 @@ function html() {
 
 function scripts() {
   return src([
-      "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/slick-carousel/slick/slick.min.js",
-      "node_modules/mixitup/dist/mixitup.min.js",
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/slick-carousel/slick/slick.js",
+      "node_modules/mixitup/dist/mixitup.js",
       "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+      "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+      "node_modules/rateyo/src/jquery.rateyo.js",
       "!app/js/main.min.js",
       "app/js/main.js",
     ])
